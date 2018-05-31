d2aafa12b3da02c7ad0c8c1e275372cbd1b16057
jamie marsh <jmarsh@Panoramas-iMac-3.local>
2018-05-31 15:38:24 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Jamie',
-        text: 'Hey, I created a message!'
-    });
 });
 
 socket.on('disconnect', function () {
