630ba51eb22b42e8a2f717e52d5bfade33663bf1
jamie marsh <jmarsh@Panoramas-iMac-3.local>
2018-06-01 16:05:59 +0100

    Add geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -12,3 +12,16 @@ describe('generateMessage', () => {
         expect(message).toContain({from, text});
     })
 })
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'Jamie';
+        var latitude = 1234;
+        var longitude = 5678;
+        var url = 'https://www.google.com/maps?q=1234,5678';
+        var message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toContain({from, url});
+    });
+});
